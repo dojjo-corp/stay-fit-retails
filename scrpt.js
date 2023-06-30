4251820b2b8871be0d30630d925aeb66661283d4
dojjo-desktop <hosuatetteh@gmail.com>
2023-06-30 08:52:34 +0000

    add to cart -i

--- scrpt.js
+++ scrpt.js
@@ -1,5 +1,76 @@
+let productList = document.getElementsByClassName("pro");
+productList = Array.from(productList);
+productList.forEach(function(item){
+    var button = item.querySelector("button");
+    button.addEventListener("click", getUserSelection);
+})
+var product = productList[0];
 
-var productList = document.querySelectorAll(".pro");
+
+// GET USER'S SELECTION
+function getUserSelection(event) {
+    var counter = 0;
+    var cartList = JSON.parse(sessionStorage.getItem('cartList')) || [];
+    var cartItem = {
+        "id": "",
+        "img": event.target.parentNode.parentNode.childNodes[1].getAttribute("src"),
+        "name": event.target.parentNode.childNodes[1].textContent,
+        "price": event.target.previousElementSibling.textContent,
+    }
+    // var id = `cart-${counter}`
+    // var imgSrc = event.target.parentNode.parentNode.childNodes[1].getAttribute("src");
+    // var itemName = event.target.parentNode.childNodes[1].textContent;
+    // var price = event.target.previousSiblingElement.textContent;
+
+    // assign property values to cart item
+    // cartItem["id"] = id;
+    // cartItem["img"] = imgSrc;
+    // cartItem["name"] = itemName;
+    // cartItem["price"] = price;
+
+    // add to cart list
+
+    cartList.push(cartItem)
+    window.sessionStorage.setItem("cartList", JSON.stringify(cartList));
+}
+
+// for add to cart button
+function addToCart() {
+    var wrapper = document.querySelector(".cart-wrapper");
+
+    // TODO: GET USER'S SELECTION LIST FROM LIST OF PRODUCTS
+    var cartItems = JSON.parse(window.sessionStorage.getItem("cartList"));
+    cartItems.forEach((item)=>{
+        var img = item.img;
+        var name = item.name;
+        var price = item.price;
+
+        var cartItem = document.createElement("div");
+        cartItem.classList.add("cart-item");
+        // add to container
+        wrapper.appendChild(cartItem);
+
+        var cartImg = document.createElement("img");
+        cartImg.classList.add("image-cart");
+        cartImg.setAttribute("src", img);
+        var cartName = document.createElement("p");
+        cartName.textContent = name;
+        var cartPrice = document.createElement("h4");
+        cartPrice.textContent = price;
+        var btn = document.createElement("button");
+        btn.classList.add("pro-btn");
+        btn.textContent = "Remove";
+        // 
+
+        cartItem.appendChild(cartImg);
+        cartItem.appendChild(cartName);
+        cartItem.appendChild(cartPrice);
+        cartItem.appendChild(btn);
+        
+        
+
+    })
+}
 
 // for search bar
 function searchProduct() {
@@ -15,18 +86,4 @@ function searchProduct() {
             product.style.display = "";
         }
     }
-}
-
-// for add to cart button
-function addToCart(button) {
-    
-    var userSelectionList = productList.querySelectorAll(".chosen");
-    // TODO: GET USER'S SELECTION LIST FROM LIST OF PRODUCTS
-    // for each button's onclick, add the "chosen" class to the product item's classList
-    // use all elements with "chosen" class as user's selection
-    // display in cart page
-
-    // loop product items and get button's onclick to push product to product list
-    // 
-    
 }
